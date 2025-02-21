baf92e657a03732ecd65db319a5d639eabf7a8d3
henry <sc23ysl@leeds.ac.uk>
2025-02-21 00:17:43 +0000

    added lag features, visualization of target result (2014, from 2010-2013), data preparation for SVR model
    next step is to split to test and train the data for SVR model. successfully converted year into numerical index as required by SVR model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # read dataset
 read_dataset = pd.read_csv('sustainable_fashion_trends_2024.csv')
@@ -8,25 +9,31 @@ column_name = 'Carbon_Footprint_MT'
 
 # check rows with missing values
 missing_values = read_dataset[column_name].isnull().sum()
-print ("Missing Values in '{column_name}': ", missing_values)
+print ("Missing Values: ", missing_values)
 
-# selected metrics for prediction
-selected_columns = ['Carbon_Footprint_MT']
+## lag features
+# target results of 2014 (2010 to 2013)
+read_dataset_lag = read_dataset[(read_dataset['Year'] >= 2010) & (read_dataset['Year'] <= 2014)]
 
-# descriptive statistics
-descriptive_statistics = read_dataset[selected_columns].describe()
+# mean carbon footprint per year
+mean_co2_per_year = read_dataset_lag.groupby('Year')['Carbon_Footprint_MT'].mean().reset_index()
 
-# calculating mean, variance, standard deviation, minimum & maximum values of selected column
-mean = read_dataset[selected_columns].mean()
-variance = read_dataset[selected_columns].var()
-standard_deviation = read_dataset[selected_columns].std()
-minimum = read_dataset[selected_columns].min()
-maximum = read_dataset[selected_columns].max()
+# convert year into index
+mean_co2_per_year['Year_Index'] = mean_co2_per_year['Year'] - mean_co2_per_year['Year'].min()
 
-# print raw descriptive statistics
-print("Descriptive Statistics: \n", descriptive_statistics)
-print("\nMean: ", mean)
-print("\nVariance: ", variance)
-print("\nStandard Deviation: ", standard_deviation)
-print("\nMinimum: ", minimum)
-print("\nMaximum: ", maximum)
+plt.figure(figsize=(8,5))
+plt.plot(mean_co2_per_year['Year'], mean_co2_per_year['Carbon_Footprint_MT'], marker='o', linestyle='-', color='b')
+
+# plotting target results of 2014 (2010 to 2013)
+plt.xlabel("Year")
+plt.ylabel("Mean Carbon Footprint (MT)")
+plt.title("Target Mean Carbon Footprint Per Year (2010-2014)")
+plt.grid(True)
+plt.show()
+
+# SVR Model preparation
+X = mean_co2_per_year[['Year_Index']].values  # numerical year index (required by SVR)
+y = mean_co2_per_year['Carbon_Footprint_MT'].values  # target values
+
+print("\nPrepared Data for SVR Model:")
+print(mean_co2_per_year)
